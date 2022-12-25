e527e1863cb9811ee700e4280a1ac4fc675187fc
Bernar Berdikul <berdikul_bg@nlmk.com>
2022-12-25 08:30:16 +0600

    [main][commit#6]: Add class for API requests.

--- tasks/data_fetching.py
+++ tasks/data_fetching.py
@@ -2,6 +2,21 @@
 
 __all__ = ('DataFetchingTask',)
 
+from dataclasses import dataclass
+from typing import ClassVar
 
+from clients.weather_api import YandexWeatherAPI
+
+
+@dataclass
 class DataFetchingTask:
-    pass
+    city_name: str
+    WEATHER_API: ClassVar[YandexWeatherAPI] = YandexWeatherAPI()
+
+    def fetch(self) -> tuple[str, dict]:
+        print(f"{self.city_name}: запрашиваем погоду в городе.")
+        resp = self.WEATHER_API.get_forecasting(city_name=self.city_name)
+        return self.city_name, resp
+
+    def __call__(self, *args, **kwargs) -> tuple[str, dict]:
+        return self.fetch()
