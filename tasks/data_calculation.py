137323ebaa4453702b60e63bd890092e02bb8565
Bernar Berdikul <berdikul_bg@nlmk.com>
2022-12-26 00:47:36 +0600

    [main][commit#12]: Add rating calculation.

--- tasks/data_calculation.py
+++ tasks/data_calculation.py
@@ -1,9 +1,27 @@
 from dataclasses import dataclass, field
 from typing import Any
+
 from utils import constants
 
-
 __all__ = ('DataCalculationTask',)
+
+
+PRECIPITATION_RATING: dict[int, list[int]] = {
+    1: [1, 2],
+    2: [3, 4],
+    3: [5, 6],
+    4: [7, 8],
+    5: [9, 10]
+}
+
+
+TEMP_RATING: dict[int, list[int]] = {
+    1: [0, 5],
+    2: [5, 10],
+    3: [10, 30],
+    4: [20, 30],
+    5: [30, 40]
+}
 
 
 @dataclass
@@ -20,7 +38,26 @@ class DataCalculationTask:
     def count_hours_without_precipitation(hours: list) -> int:
         return len([h for h in hours if h['condition'] in constants.WEATHER_WITHOUT_PRECIPITATION])
 
-    def calculation(self) -> dict[str, dict[str, Any]]:
+    @staticmethod
+    def calculate_rating(average_week_temp: float, average_without_precipitation: float) -> int:
+        average_week_temp_rating: int = 0
+        average_without_precipitation_rating: int = 0
+        for rate, (l_border, r_border) in PRECIPITATION_RATING.items():
+            if average_without_precipitation == 0:
+                break
+            if l_border < average_without_precipitation <= r_border:
+                average_without_precipitation_rating = rate
+                break
+        for rate, (l_border, r_border) in TEMP_RATING.items():
+            if average_week_temp == 0:
+                break
+            if l_border < average_week_temp <= r_border:
+                average_week_temp_rating = rate
+                break
+        rating = average_week_temp_rating + average_without_precipitation_rating
+        return rating
+
+    def calculation(self) -> dict[str, Any]:
         week_day_temp: list[float] = []
         week_day_hours_without_precipitation: list[int] = []
         for forecast in self.forecasts:
@@ -45,15 +82,20 @@ class DataCalculationTask:
                     "average_day_temp": average_day_temp,
                 }
             )
+        average_week_temp: float = self.average(data=week_day_temp)
+        average_without_precipitation: float = self.average(data=week_day_hours_without_precipitation)
+        rating: int = self.calculate_rating(
+            average_week_temp=average_week_temp,
+            average_without_precipitation=average_without_precipitation,
+        )
+        print(f"[{self.city_name}]: Получен рейтинг города = {rating}.")
         return {
-            self.city_name: {
-                "city_name": self.city_name,
-                "days": self.days,
-                "average_week_temp": self.average(data=week_day_temp),
-                "average_without_precipitation": self.average(data=week_day_hours_without_precipitation),
-                "rating": "-",
-                "forecasts": self.forecasts,
-            }
+            "city_name": self.city_name,
+            "days": self.days,
+            "average_week_temp": average_week_temp,
+            "average_without_precipitation": average_without_precipitation,
+            "rating": rating,
+            "forecasts": self.forecasts,
         }
 
     def __call__(self, *args, **kwargs) -> dict[str, dict[str, Any]]:
